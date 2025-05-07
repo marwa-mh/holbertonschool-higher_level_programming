0a2ab982d89bde3e299315fa1d40ceb0601bf0c0
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-07 19:41:42 +1000

    change islower function and use and operator

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-islower = __import__('7-islower').islower
-
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    return False
 
 def uppercase(str):
     for c in str:
