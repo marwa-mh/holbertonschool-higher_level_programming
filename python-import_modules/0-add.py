7c635277a35c33da19ae60f47582142a450dc579
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-08 16:00:14 +1000

    Add main() to the file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 add = __import__('add_0').add
 
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}".format())
+
+def main():
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}".format())
+
+
+if __name__ == "__main__":
+    main()
