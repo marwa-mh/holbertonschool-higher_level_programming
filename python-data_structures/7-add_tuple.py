96dd2e881951380fa2a8b4360883f982210b3130
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-13 08:09:33 +1000

    fix add_tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     res = []
-    max_length = min(len(tuple_a), len(tuple_b))
-    if max_length > 2:
-        max_length = 2
-    for i in range(0, max_length):
-        res.append(tuple_a[i] + tuple_b[i])
-    if max_length < 2:
-        if len(tuple_b) > max_length:
-            for i in range(max_length, 2):
-                res.append(tuple_b[i])
-        if len(tuple_a) > max_length:
-            for i in range(max_length, 2):
-                res.append(tuple_a[i])
-    res = tuple(res)
+    tuple_a = tuple(list(tuple_a)[:2] + [0] * (2 - len(tuple_a)))
+    tuple_b = tuple(list(tuple_b)[:2] + [0] * (2 - len(tuple_b)))
+    res = tuple(map(sum, zip(tuple_a, tuple_b)))
     return res
