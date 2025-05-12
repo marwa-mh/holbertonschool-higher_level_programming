3b76b5ac3e9e310d3a123db0adcee6bdebe52a43
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-13 08:29:41 +1000

    fix max_number to handle negative values

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_number = 0
-    for n in my_list:
+    max_number = my_list[0]
+    for n in my_list[1:]:
         max_number = n if max_number < n else max_number
     return max_number
