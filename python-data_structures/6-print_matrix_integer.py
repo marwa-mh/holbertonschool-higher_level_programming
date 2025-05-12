99d1f6b599bad9de765edb28887beed81bbfc4ad
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-12 15:01:16 +1000

    Add format function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(*row)
+        format_string = " ".join('{:d}'.format(x) for x in row)
+        print(format_string)
+    
