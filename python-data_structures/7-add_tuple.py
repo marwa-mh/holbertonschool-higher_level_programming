e9ed81641f3c746c9a93bfa86d811f3769992f28
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-13 07:56:24 +1000

    fix add_tuple to add just 2 elements

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,13 +2,16 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     res = []
     max_length = min(len(tuple_a), len(tuple_b))
+    if max_length > 2:
+        max_length = 2
     for i in range(0, max_length):
         res.append(tuple_a[i] + tuple_b[i])
-    if len(tuple_b) > max_length:
-        for i in range(max_length, len(tuple_b)):
-            res.append(tuple_b[i])
-    if len(tuple_a) > max_length:
-        for i in range(max_length, len(tuple_a)):
-            res.append(tuple_a[i])
+    if max_length < 2:
+        if len(tuple_b) > max_length:
+            for i in range(max_length, 2):
+                res.append(tuple_b[i])
+        if len(tuple_a) > max_length:
+            for i in range(max_length, 2):
+                res.append(tuple_a[i])
     res = tuple(res)
     return res
--- python-data_structures/7-main.py
+++ python-data_structures/7-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 add_tuple = __import__('7-add_tuple').add_tuple
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
+tuple_a = (1, 2)
+tuple_b = (1, 2,3)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
