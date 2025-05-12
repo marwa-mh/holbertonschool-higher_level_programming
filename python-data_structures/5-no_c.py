adf20f642fe1854a0cb846210eabd203183e644b
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-12 13:12:06 +1000

    Add 5-no_c.py using tranlate built-in

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def no_c(my_string):   
+    remove_chars="cC"
+    translation_table = my_string.maketrans("","", remove_chars)
+    my_string=my_string.translate(translation_table)
+    return my_string
