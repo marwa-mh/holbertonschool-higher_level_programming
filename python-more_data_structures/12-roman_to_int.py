35ec6e200b82f0d6e9a4af1930186fe3ebaef134
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-13 12:55:37 +1000

    fix condition

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     sum = 0
     found_I = False
     for s in roman_string:
         if s == 'I':
-            found_I = True           
-        elif (s == 'V' or s == 'X') and found_I == True:
-            sum = sum - 2 
+            found_I = True
+        elif (s == 'V' or s == 'X') and found_I:
+            sum = sum - 2
             found_I = False
         else:
             found_I = False
         sum = sum + roman.get(s)
-       
     return sum
-
-
