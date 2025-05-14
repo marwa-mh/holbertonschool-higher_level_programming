14a49e082b50e9865f190b96cd6e18c6d1f5bbdc
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-14 12:23:56 +1000

    Add 3-safe_print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = ""
+    try:
+        x = a / b
+        result = "{}".format(a / b)
+    except ():
+        result = "None"
+    finally:
+        print(f"Inside result: {result}")
+        return result
