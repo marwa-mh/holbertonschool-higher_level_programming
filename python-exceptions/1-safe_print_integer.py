2c3c9bd3af66d89a7ce54e0db4b748ae7e7a3eb3
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-14 10:03:04 +1000

    Add TypeError to except

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(value):
         d = int(value)
         print("{:d}".format(d))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
