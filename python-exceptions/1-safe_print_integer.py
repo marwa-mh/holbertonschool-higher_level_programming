5ce4f9c3b09847741a75e4ebc1e55e33d439ece8
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-14 10:00:49 +1000

    Add 1-safe_print_integer using ValueError exception

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        d = int(value)
+        print("{:d}".format(d))
+        return True
+    except ValueError:
+        return False
