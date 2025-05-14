4627386338d936065ec5abf26f89ec9cde798f27
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-14 12:51:13 +1000

    Add 4-list_division

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for i in range(list_length):
+        x = 0
+        try:
+            x = my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError):
+            print("division by 0")
+        except (ValueError, TypeError):
+            print("wrong type")
+        except (IndexError):
+            print("out of range")
+        finally:
+            result_list.append(x)
+    return result_list
