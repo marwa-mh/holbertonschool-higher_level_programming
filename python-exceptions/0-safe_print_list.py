f7d23345ee85afc57442ad6aeaee42902358f257
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-14 11:55:30 +1000

    Add 2-safe_print_list_integers.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(f"{my_list[i]}".format(), end="")
         print()
-        return x
+        return i + 1
     except IndexError:
         print()
         return x if x <= i else i
