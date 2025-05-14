cada18785bdd6a69f36c84ffec97ab73d6b84499
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-14 10:11:51 +1000

    fix x returned when error happened

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        i = 0
         for i in range(x):
             print(f"{my_list[i]}".format(), end="")
         print()
+        return i + 1
     except IndexError:
         print()
-    return x
+        return x
