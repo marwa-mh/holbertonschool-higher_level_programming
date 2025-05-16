6bb38578ccc3f27f0e0739d55394ecd059158b6a
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-16 12:20:46 +1000

    fix documentation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,6 +5,9 @@ this module provide a function print_square
 
 
 def print_square(size):
+    """
+    this function print square of #
+    """
     err_mes_int = "size must be an integer"
     if not isinstance(size, int) or (isinstance(size, float) and size < 0):
         raise TypeError(err_mes_int)
