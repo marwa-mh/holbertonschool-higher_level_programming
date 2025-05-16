d0fb67085eeae4cf8456c6e1a0d94df907facc0d
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-16 19:28:42 +1000

    Add if statement to check 0 length

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0.0
     weight = sum(x[1] for x in my_list)
     mul = sum(x[0] * x[1] for x in my_list)
     return mul / weight
