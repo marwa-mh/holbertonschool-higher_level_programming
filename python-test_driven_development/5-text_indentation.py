5435487e3529cb8118c0df6b1237ea8b7a7c9d69
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-16 13:29:52 +1000

    fix style

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -18,7 +18,7 @@ def text_indentation(text):
         result += text[i]
         if text[i] in ('.', ':', '?'):
             result = result + '\n\n'
-            while (i + 1 < len(text) and text[i + 1] == ' ' ):
+            while (i + 1 < len(text) and text[i + 1] == ' '):
                 i += 1
         i += 1
     print(result.strip(), end="")
