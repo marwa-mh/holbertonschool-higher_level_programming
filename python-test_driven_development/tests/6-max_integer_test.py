29e619aa75663e8a5473c8cee74dcea873a8adb4
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-16 19:07:10 +1000

    add some status for test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,3 +7,11 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_values(self):
         self.assertAlmostEqual(max_integer([2,3,5]), 5)
+        self.assertAlmostEqual(max_integer([1,3,2]), 3)
+        self.assertAlmostEqual(max_integer([9,3,2]), 9)
+        self.assertAlmostEqual(max_integer([1,-3,2]), 2)
+        self.assertAlmostEqual(max_integer([-1,-3,-2]), -1)
+        self.assertAlmostEqual(max_integer([2]), 2)
+        self.assertAlmostEqual(max_integer([]), None)
+
+
