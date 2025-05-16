4958e9d2dfefd49da8be9690f57ac5c7a9327b03
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-17 09:41:08 +1000

    Add 100-safe_print_iteger

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ():
-        print(f"Exception: {sys.stderr}".format())
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         return False
