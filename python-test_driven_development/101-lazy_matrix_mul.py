1b281af594459e8cf89f4efd99461b2fc02ffcd9
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-18 10:49:30 +1000

    add all conditions

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -12,10 +12,30 @@ def lazy_matrix_mul(m_a, m_b):
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")  
+        raise TypeError("m_b must be a list")
     if not all(isinstance(ele, list) for ele in m_a):
-       raise TypeError("m_a must be a list of lists")
+        raise TypeError("m_a must be a list of lists")
     if not all(isinstance(ele, list) for ele in m_b):
         raise TypeError("m_b must be a list of lists")
+    ma_col_len = len(m_a[0])
+    if ma_col_len == 0:
+        raise ValueError("m_a can't be empty")
+    for row in m_a:
+        if len(row) !=ma_col_len:
+            raise TypeError("each row of m_a must be of the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    mb_col_len = len(m_b[0])
+    if mb_col_len == 0:
+        raise ValueError("m_b can't be empty")
+    for row in m_b:
+        if len(row) !=mb_col_len:
+            raise TypeError("each row of m_b must be of the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
     result = np.dot(m_a, m_b)
     return result
