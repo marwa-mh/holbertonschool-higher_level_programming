7d7624f1118fbe8e8b7751f08e7431f6b325d909
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-21 09:17:41 +1000

    Add 2-rectangle.py

--- python-more_classes/2-main.py
+++ python-more_classes/2-main.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 """
-Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
-
-Private instance attribute: width:
-property def width(self): to retrieve it
-property setter def width(self, value): to set it:
-width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-if width is less than 0, raise a ValueError exception with the message width must be >= 0
-Private instance attribute: height:
-property def height(self): to retrieve it
-property setter def height(self, value): to set it:
-height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-if height is less than 0, raise a ValueError exception with the message height must be >= 0
-Instantiation with optional width and height: def __init__(self, width=0, height=0):
+Public instance method: def area(self): that returns the rectangle area
+Public instance method: def perimeter(self): that returns the rectangle perimeter:
+if width or height is equal to 0, perimeter is equal to 0
 """
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('2-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print("--")
 
 my_rectangle.width = 10
 my_rectangle.height = 3
-print(my_rectangle.__dict__)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-"""guillaume@ubuntu:~/$ ./1-main.py
-{'_Rectangle__width': 2, '_Rectangle__height': 4}
-{'_Rectangle__width': 10, '_Rectangle__height': 3}"""
+"""guillaume@ubuntu:~/$ ./2-main.py
+Area: 8 - Perimeter: 12
+--
+Area: 30 - Perimeter: 26"""
---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This module contain a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class is empty
+    """
+    @classmethod
+    def __validate(self, width, height):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+    def __init__(self, width=0, height=0):
+        Rectangle.__validate(width, height)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__validate(value, 0)
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__validate(0, value)
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return 2 * (self.__height + self.__width)
