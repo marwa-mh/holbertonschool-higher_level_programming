c25cc9d7ed27895518b8e56b204c272dc83d645c
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-20 12:08:23 +1000

    Add -square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+this module provides a square class
+"""
+
+
+class Square:
+    """
+    contain a function __init__
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple) or not all(isinstance(e,int) for e in position) or not len(position) == 2 or len(list(x for x in position if x < 0)) > 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+     #   if not isinstance(value, tuple(int, int)):
+      #      raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        printed:bool = False
+        if self.size == 0:
+            print()
+        for r in range(self.size):
+            if self.position[0] > 0:
+                    for k in range(self.position[0]):
+                        print("_", end="")
+            for c in range(self.size):               
+                print("#", end="")
+            print()
