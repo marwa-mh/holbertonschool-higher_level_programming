6510efe38eb2b4ec1ae1c3e76d81fc1cc6c10572
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-20 10:35:25 +1000

    Add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this module provides a square class
+"""
+
+
+class Square:
+    """
+    contain a function __init__
+    """
+    def __init__(self, size=""):
+        self.__size = size
