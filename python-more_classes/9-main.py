2abd2ee4726dde111c449898815405e564ad46b4
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-21 12:18:50 +1000

    Add 9-rectangle.py

---
+++ python-more_classes/9-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+my_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+print(my_square)
+
+"""guillaume@ubuntu:~/$ ./9-main.py
+Area: 25 - Perimeter: 20
+#####
+#####
+#####
+#####
+#####
+Bye rectangle..."""
+"""
+Class method def square(cls, size=0): that returns a new
+ Rectangle instance with width == height == size
+"""
