6a7bd497c97980f0b3642bdee2a8cb90cd595bea
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-26 18:01:41 +1000

    8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+Module contains class BaseGeometery
+                instance method : area
+"""
+
+
+class Rectangle(BaseGeometry):
+    """init method"""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
