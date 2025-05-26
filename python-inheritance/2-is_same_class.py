31ce7c7462c79c800cdeb7a46770cfcc5506b9ca
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-26 15:05:27 +1000

    fix the condition using __name__

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@ def is_same_class(obj, a_class):
     """
     check if the obj is same type of aclass
     """
-    if isinstance(type(obj), a_class):
+    if type(obj).__name__ == a_class.__name__:
         return True
     return False
