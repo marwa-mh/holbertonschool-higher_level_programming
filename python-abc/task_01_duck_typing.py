b5c9fab01ee4a715fdb0c566ee0475f802ad06a1
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-27 09:06:08 +1000

    Handle perimeter negative value

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -27,7 +27,7 @@ class Circle(Shape):
         return math.pi * math.pow(self.__radius, 2)
 
     def perimeter(self):
-        return 2 * math.pi * self.__radius
+        return 2 * math.pi * abs(self.__radius)
 
 
 class Rectangle(Shape):
