43ebc800218e8f8c615bcf19a7216847482d3025
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-26 13:13:26 +1000

    return the list in addition to print it

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,4 +9,6 @@ class MyList(list):
     contain instance function print_sorted
     """
     def print_sorted(self):
-        print(sorted(self))
+        sorted_list = sorted(self)
+        print(sorted_list)
+        return sorted_list
