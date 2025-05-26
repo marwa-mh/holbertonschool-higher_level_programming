acd176431710de02b96583e347d2c1a5c69f830f
Marwa-m <marwa.almahmoud@hotmail.com>
2025-05-26 16:00:11 +1000

    Add method area() without implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module contains class BaseGeometery
+                instance method : area
+"""
+
+
+class BaseGeometry:
+    """instance method area"""
+    def area(self):
+        raise Exception("area() is not implemented")
