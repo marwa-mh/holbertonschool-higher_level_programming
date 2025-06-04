959e01bb874e2b42b14be4f1f4e32a42ad5c0777
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-05 07:36:01 +1000

    fix pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,15 +3,15 @@
 import json
 import sys
 from os import path
-save_to_json_file =__import__('5-save_to_json_file').save_to_json_file
-load_from_json_file =__import__('6-load_from_json_file').load_from_json_file
-data:list
-filename ='add_item.json'
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+data: list
+filename = 'add_item.json'
 
 if path.exists(filename):
     try:
         data = load_from_json_file(filename)
-    except:
+    except Exception as e:
         data = []
 else:
     data = []
