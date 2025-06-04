93a6f1abdfd8bb65331f69773ae43713b52bcb3b
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-04 15:46:07 +1000

    Add 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         f.close()
-        print(read_data,end="")
+        print(read_data, end="")
