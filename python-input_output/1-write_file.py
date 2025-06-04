8548253bcbf8ce832cb517c4a16fec91a6a04a76
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-04 16:11:31 +1000

    change the mode of the file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,7 @@ def write_file(filename="", text=""):
     """
     function write text to file
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename,'w', encoding="utf-8") as f:
         num_of_chr = f.write(text)
         f.close()
         print(num_of_chr, end="")
