3736ceb8e3f08b4f6682af42d680121a8d4f0ba4
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-04 20:28:00 +1000

    Add 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns the JSON representation of an object (string): """
+import json
+
+
+def to_json_string(my_obj):
+    """
+    convert obj to string
+    """
+    return json.dumps(my_obj)
