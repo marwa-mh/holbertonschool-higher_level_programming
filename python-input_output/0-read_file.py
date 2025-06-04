91daac78bd305c98866302a4004ca9831436324f
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-04 15:37:47 +1000

    Add 0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function reads text """
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        f.close()
+        print(read_data)
