b203f8d0ccd23aa542afd001946525b99dbdc801
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-06 08:49:18 +1000

    repalce pickle to json in task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,17 +2,17 @@
 """Module contains 2 functions
 serialize_and_save_to_file
 load_and_deserialize"""
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """serialize the object and save it to file"""
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """deserialize data from file and return"""
-    with open(filename, "rb") as file:
-        data = pickle.load(file)
+    with open(filename, "r") as file:
+        data = json.load(file)
     return data
