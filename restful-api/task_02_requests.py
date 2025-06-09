708e6a6d85996ea01561f1b5f48e4477cb0189ce
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-09 18:32:19 +1000

    change the url from todo to posts

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,7 +7,7 @@ import csv
 
 def fetch_and_print_posts():
     """fetches all post from JSONPlaceholder. and print it"""
-    url = 'https://jsonplaceholder.typicode.com/todos'
+    url = 'https://jsonplaceholder.typicode.com/posts'
     rq = requests.get(url)
     status_code = rq.status_code
     print(f"Status Code: {status_code}")
@@ -19,7 +19,7 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     """fetches all post from JSONPlaceholder. and save it to csv file"""
-    url = 'https://jsonplaceholder.typicode.com/todos'
+    url = 'https://jsonplaceholder.typicode.com/posts'
     rq = requests.get(url)
     status_code = rq.status_code
     if status_code == requests.codes.ok:
@@ -27,9 +27,9 @@ def fetch_and_save_posts():
         dic: dict
         list_of_posts: list[dict] = [{}]
         for post in posts:
-            dic = {'id': post['id'], 'title': post['title']}
+            dic = {'id': post['id'], 'title': post['title'], 'body': post['body']}
             list_of_posts.append(dic)
-        fieldnames = ['id', 'title']
+        fieldnames = ['id', 'title', 'body']
         with open('posts.csv', 'w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
