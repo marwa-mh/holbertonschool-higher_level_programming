5ad94e798717bb685e38f67047e7a24b9c0d7b04
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-24 19:18:23 +1000

    2 create database and user with select privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create User
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE User IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
