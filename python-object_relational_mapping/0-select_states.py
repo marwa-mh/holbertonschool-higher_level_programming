b458068ce474aeb1e972ff26a5b00cbc84db69fb
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-26 10:07:34 +1000

    Add 0-Get all state using python and MySQLdb

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/user/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname,
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
