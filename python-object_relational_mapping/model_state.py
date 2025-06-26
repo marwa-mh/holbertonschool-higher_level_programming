ff771b1623e77ff61d091c93a199364bfe3ee048
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-27 09:11:23 +1000

    Add __main__to modelstate.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,19 +2,32 @@
 """
 Class state using SQLAlchemy
 """
+
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-# Connecting to MySQL server at 23.92.23.113 using mysql-python DBAPI 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "hbtn_0c_0"), pool_pre_ping=True)
+
+
+"""
+Class state using SQLAlchemy
+"""
 
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column("id", Integer, autoincrement="auto", primary_key=True, unique=True, nullable=False)
+    id = Column("id", Integer,
+                autoincrement="auto",
+                primary_key=True,
+                unique=True,
+                nullable=False)
     name = Column("name", String(128), nullable=False)
 
-Base.metadata.create_all(engine)
-                
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
