a4534a19c3dddaf42106b47306903886ab349822
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-27 11:29:32 +1000

    fetch the first state using first()

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Model has a class state
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, Session
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    print the first state of object from yhe database
+    """
+    __tablename__ = 'states'
+    id = Column("id", Integer,
+                autoincrement="auto",
+                primary_key=True,
+                unique=True,
+                nullable=False)
+    name = Column("name", String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
