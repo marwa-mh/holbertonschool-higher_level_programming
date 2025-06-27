11dcd6ea8e79bdfebc61c434dc15ce3af3bcd4c2
Marwa-m <marwa.almahmoud@hotmail.com>
2025-06-27 11:34:28 +1000

    return all state with letter a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module fetch all States contain letter a
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, Session
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class fetch all States contain letter a
+    """
+    __tablename__ = 'states'
+    id = Column("id", Integer,
+                autoincrement="auto",
+                primary_key=True,
+                unique=True,
+                nullable=False)
+    name = Column("name", String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).where(
+            State.name.contains('a')).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
