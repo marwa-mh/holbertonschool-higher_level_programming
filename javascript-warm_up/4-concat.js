45c82b6a54f4ec30da8aec95fc299bc07e26831e
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-18 17:03:31 +1000

    parseINT

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,3 +3,4 @@
 const { argv } = require('node:process');
 
 console.log(`${argv[2]} is ${argv[3]}`);
+
---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (parseInt(argv[2])){
+console.log(`My number: ${argv[2]}`);
+}else{
+    console.log('Not a number');
+}
