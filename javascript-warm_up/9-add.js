751a806b43610090378be62e95a3dd4dad456f30
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-18 18:14:22 +1000

    Add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+const { argv } = require('node:process');
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+
+console.log(add(a, b));
